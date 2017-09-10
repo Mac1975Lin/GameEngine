38adfe3ee82b305a9a50ee8b1a316c55673e0e7e
MacLin <mac1975.lin@yahoo.com>
2017-09-10 23:57:44 +0800

    123

---
+++ bin2c/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fptr = fopen(argv[1], "rb");
+	if(!fptr)
+	{
+		printf("Usage: bin2c infile\n");
+		return -1;
+	}
+
+	fseek(fptr, 0, SEEK_END);
+	int flength = ftell(fptr);
+	fseek(fptr, 0, SEEK_SET);
+
+	int idx = 0;
+	for(int i=0; i<flength; i++)
+	{
+		unsigned char ch;
+		fread(&ch, 1, 1, fptr);
+		printf("0x%02X, ", ch);
+		idx++;
+		if(idx==16)
+		{
+			printf("\n");
+			idx= 0;
+		}
+	}
+
+	fclose(fptr);
+
+	return 0;
+}
